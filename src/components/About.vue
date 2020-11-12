b61c7cfcc398782f5e3f156430b45fbbaeaef61c
RicardoSerranoPena <ricardo30499@gmail.com>
2020-11-12 19:35:25 +0700

    Added profile photo to about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,7 +6,15 @@
         <span></span>
         <span></span>
       </div>
-      <p>Born in El Salvador <br>I have a passion for technology and learning</p>
+      <p>Born in El Salvador<br>
+        Raised in Los Angeles<br>
+        Living in Vietnam!<br>
+        I have a passion for <br>
+        technology and learning
+      </p>
+    </div>
+    <div class="portrait-container">
+      <img alt="self-portrait" src="../assets/images/Ricardo.png">
     </div>
   </div>
 </template>
@@ -18,5 +26,19 @@ export default {
 </script>
 
 <style scoped>
+  .portrait-container {
+    height: 100vh;
+    margin: 0 8% 0 auto;
+    position: relative;
+    display: block;
+    font-size: 1.5rem;
+    overflow: hidden;
+  }
+  img {
+    height: 100vh;
+    -webkit-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+    -moz-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+    box-shadow: 2px 15px -12px rgba(0, 0, 0, 0.57);
+  }
 
 </style>
--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="full-page dark contact" id="contact">
     <div class="title">
-      <h1>Say Hello!</h1>
+      <h1>Say Hi!</h1>
       <div class="border">
         <span></span>
         <span></span>
       </div>
       <a href="mailto:ricardo30499@gmail.com">ricardo30499@gmail.com</a>
+    </div>
+    <div class=form-container>
+      
     </div>
   </div>
 </template>
